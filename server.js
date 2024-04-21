e3093939bc9ee64edc54714d4ec61c77ea1b230e
dfreeman3102 <davidfreeman3102@gmail.com>
2024-04-21 17:34:30 -0400

    added get req to access the db.json file data

--- server.js
+++ server.js
@@ -1,20 +1,41 @@
 const express = require("express");
-const path = require('path');
+const path = require("path");
 const { env } = require("process");
-
+const fs = require("fs");
+// Helper method for generating unique ids
+const uuid = require('./helpers/uuid');
+//intializes express
 const app = express();
-
+//sets the port
 const PORT = process.env.PORT || 3001;
 
 // Express middleware
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+//sets the homepage to be the index.html file
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+//sets the notes page to be the notes.html file
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
+);
+//gets the data to be sent to the notes page
+app.get('/api/notes', (req, res) => {
+     // read existing notes
+     fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+          // console out data
+          console.log(data);
+          const notes = JSON.parse(data);
+      };
+    // Log the request to the terminal
+    console.log(`${req.method} request received to get old notes`);
+    })
+  });
+  
+  // POST request to add notes
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
-
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
-
-app.listen(PORT, () => 
-console.log(`Server running at http://localhost:${PORT}`)
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`)
 );
