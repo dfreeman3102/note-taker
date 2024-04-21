30efd4285a3ee85b4b624d78c3c5420e3ea2102d
dfreeman3102 <davidfreeman3102@gmail.com>
2024-04-21 16:05:16 -0400

    started server file, able to launch the server with index popping up on command

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require('path');
+const { env } = require("process");
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+// Express middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+app.listen(PORT, () => 
+console.log(`Server running at http://localhost:${PORT}`)
+);
