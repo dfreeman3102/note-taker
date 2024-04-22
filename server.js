46859e14c7d5a6927e5e09c818df37ee76ff181b
dfreeman3102 <davidfreeman3102@gmail.com>
2024-04-21 20:38:27 -0400

    added function to make notes save in db.json, need to make data appear on screen

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const { env } = require("process");
 const fs = require("fs");
 // Helper method for generating unique ids
-const uuid = require('./helpers/uuid');
+const uuid = require("./helpers/uuid");
 //intializes express
 const app = express();
 //sets the port
@@ -20,22 +20,50 @@ app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "public/notes.html"))
 );
 //gets the data to be sent to the notes page
-app.get('/api/notes', (req, res) => {
-     // read existing notes
-     fs.readFile('./db/db.json', 'utf8', (err, data) => {
+app.get("/api/notes", (req, res) => {
+  // read existing notes
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // console out data
+      console.log(data);
+    }
+    // Log the request to the terminal
+    console.log(`${req.method} request received to get old notes`);
+  });
+});
+
+// POST request to add notes
+app.post("/api/notes", (req, res) => {
+  console.log("Received request to add a note");
+
+  const { title, text, id } = req.body;
+  //requried properties present
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
+    //reading existing data
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
       if (err) {
         console.log(err);
       } else {
-          // console out data
-          console.log(data);
-          const notes = JSON.parse(data);
-      };
-    // Log the request to the terminal
-    console.log(`${req.method} request received to get old notes`);
-    })
-  });
-  
-  // POST request to add notes
+        //convert note to a JSON object
+        const storedNotes = JSON.parse(data);
+        //add new note
+        storedNotes.push(newNote);
+        //update notes file
+        fs.writeFile("./db/db.json", JSON.stringify(storedNotes, null, 4), (err) =>
+          err ? console.log(err) : console.log("New note added successfully!")
+        );
+      }
+    });
+  }
+});
 
-app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`)
+app.listen(PORT, () =>
+  console.log(`Server running at http://localhost:${PORT}`)
 );
